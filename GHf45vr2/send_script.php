eda185c96d62da7d59145fc9b4d05e9e3562c8db
Amirhosein Ataei <ataeiamirhosein@gmail.com>
2020-11-15 18:41:23 +0100

    Add files via upload

---
+++ GHf45vr2/send_script.php
@@ -0,0 +1,15 @@
+<?php 
+if (isset($_POST['send_message_btn']))
+{
+
+mail("iotconet@gmail.com","notify","class start");
+echo 'Email Sent';
+
+}
+?>
+<html>
+<body bgcolor="yellow">
+<br/>
+<a href="https://www.iotco.net/GHf45vr2">return</a>
+</body>
+</html>
